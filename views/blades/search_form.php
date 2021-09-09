387646bf189b2690da8a0c005d6e422704dd7ba8
DevLexicon <contact@devlexicon.ca>
2021-09-09 16:47:49 -0700

    Prep for CV1.0B2

--- views/blades/search_form.php
+++
@@ -1,7 +0,0 @@
-  <form action="../search" method="post" id="searchForm">
-    <input type="search" name="search" placeholder="Search">
-    <label>
-                  <input type="submit" name="submit" style="display:none;" />
-                  <?php echo file_get_contents($baseFileURL."/ui/videoUI/searchVector.svg"); ?>
-                </label>
-  </form>
